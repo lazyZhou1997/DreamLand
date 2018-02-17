9823fef4e5560179ef6b2ceeab587bbd1ac260f6
zhouMac <LazyZhou1997@gmail.com>
2018-02-17 19:51:43 +0800

    2017-2-17 初始化项目

---
+++ Assets/Script/TestRay2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;  
+using System.Collections;  
+  
+public class TestRay2 : MonoBehaviour
+{
+	private Vector3 orgin = new Vector3(0,0,0);  
+	private RaycastHit hit;
+	
+	void Update () {
+		for (int i = 0; i < 100; i++)
+		{
+
+			// 以摄像机所在位置为起点，创建一条向下发射的射线  
+			Ray ray = new Ray(orgin, -Vector3.up); 
+		
+			if(Physics.Raycast(ray, out hit, Mathf.Infinity))  
+			{  
+				// 如果射线与平面碰撞，打印碰撞物体信息  
+				Debug.Log("碰撞对象: " + hit.collider.name);  
+				// 在场景视图中绘制射线  
+				Debug.DrawLine(ray.origin, hit.point, Color.red); 
+			}
+			
+			orgin += new Vector3(0,0,1);
+		}
+
+	}  
+}
