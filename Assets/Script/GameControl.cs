95ce8287efab872f89bd8b188e00c95115780bc7
lazyZhou1997 <LazyZhou1997@gmail.com>
2018-02-22 10:08:55 +0800

    2018-2-22 完成主菜单，story1，设置等场景

---
+++ Assets/Script/GameControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//退出游戏
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
