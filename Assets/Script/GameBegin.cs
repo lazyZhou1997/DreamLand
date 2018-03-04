a3d629aae6107bb5d86a4ca9f284ff2a236d3488
lazyZhou1997 <LazyZhou1997@gmail.com>
2018-03-04 09:29:02 +0800

    2018-3-4

--- Assets/Script/GameBegin.cs
+++ Assets/Script/GameBegin.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameBegin : MonoBehaviour
 {
 
 	//计时器，统计游戏进行的时间
 	private float _timer;
+
+	//显示当前时间的Text
+	public Text ShowTimeText;
+
+	/// <summary>
+	/// 获取当前游戏已经经过的时间
+	/// </summary>
+	public float Timer
+	{
+		get { return _timer; }
+	}
 
 	//开始计时标志
 	private bool _beginGame;
@@ -25,6 +37,7 @@ public class GameBegin : MonoBehaviour
 		//如果游戏已经开始
 		if (_beginGame)
 		{
+			//计算已经经过的时间
 			_timer += Time.deltaTime;
 		}
 	}
@@ -37,6 +50,7 @@ public class GameBegin : MonoBehaviour
 	{
 		//开始进行游戏
 		_beginGame = true;
+		Debug.Log("开始");
 	}
 
 
--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOver : MonoBehaviour {
+public class GameOver : MonoBehaviour
+{
 
-
+	//游戏开始的脚本
+	private GameBegin _gameBegin;
 
 	// Use this for initialization
 	void Start () {
-		
+		//初始化游戏开始脚本
+		_gameBegin = GetComponent<GameBegin>();
 	}
 	
 	// Update is called once per frame
@@ -18,6 +21,9 @@ public class GameOver : MonoBehaviour {
 
 	private void OnTriggerEnter(Collider other)
 	{
-		throw new System.NotImplementedException();
+		//获取游戏运行的时间
+		float totalTime = _gameBegin.Timer;
+
+		Debug.Log(totalTime);
 	}
 }
