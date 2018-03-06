5ae4939d164c6e835a830d669aa36b5ba63b98b0
zhouMac <LazyZhou1997@gmail.com>
2018-03-06 16:30:34 +0800

    2018-3-6增减光线方向指示器和修复bug

--- Assets/Script/LightIndicator.cs
+++ Assets/Script/LightIndicator.cs
@@ -8,15 +8,15 @@ public class LightIndicator : MonoBehaviour
 	//平行光对象
 	public GameObject DirectionLight;
 	
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		//光线方向
+		Vector3 direction = DirectionLight.transform.forward;
+		
 		//修改当前对象的方向与平行光对象方向相同
-		transform.LookAt(DirectionLight.transform.forward);
+		transform.LookAt(new Vector3(transform.position.x + direction.x, transform.position.y+direction.y, transform.position.z + direction.z));
+		
 	}
 }
--- Assets/Script/RotateByTouch.cs
+++ Assets/Script/RotateByTouch.cs
@@ -7,43 +7,20 @@ public class RotateByTouch : MonoBehaviour {
 
 	//转动速度
 	public int Speed;
-	
 
+
+
+	
 	protected virtual void OnEnable()
 	{
-		// Hook events
-//		LeanTouch.OnFingerDown += FingerDown;
 		LeanTouch.OnFingerSet  += FingerSet;
-//		LeanTouch.OnFingerUp   += FingerUp;
 	}
 
 
 	protected virtual void OnDisable()
 	{
-		// Unhook events
-//		LeanTouch.OnFingerDown -= FingerDown;
 		LeanTouch.OnFingerSet  -= FingerSet;
-//		LeanTouch.OnFingerUp   -= FingerUp;
 	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-//	/// <summary>
-//	/// 指头按下的时候，记录第一个指头的位置
-//	/// </summary>
-//	/// <param name="obj"></param>
-//	private void FingerDown(LeanFinger obj)
-//	{
-//		_lastFinger = obj;
-//	}
 
 	/// <summary>
 	/// 当指头保持在屏幕上时，控制TransForm进行旋转
@@ -112,13 +89,4 @@ public class RotateByTouch : MonoBehaviour {
 			return digit;
 		}
 	}
-
-//	/// <summary>
-//	/// 指头离开屏幕的时候，清空指头记录
-//	/// </summary>
-//	/// <param name="obj"></param>
-//	private void FingerUp(LeanFinger obj)
-//	{
-//		_lastFinger = null;
-//	}
 }
